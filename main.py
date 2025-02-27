f28f0d51d48c1ed7a64a0867259c49976f5104a2
Felipe Silva <felipeosilva@hotmail.com>
2025-02-26 22:48:47 -0300

    feat: jwt authentication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from controllers import post
+from controllers import post, auth
 from contextlib import asynccontextmanager
 from database import database, metadata, engine
 
@@ -16,3 +16,4 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(post.router)
+app.include_router(auth.router)
--- security.py
+++ security.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
 import time
+from typing import Annotated
 from uuid import uuid4
+
 import jwt
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import HTTPBearer
+from pydantic import BaseModel
+
+SECRET = "my-secret"
+ALGORITHM = "HS256"
 
 
 class AccessToken(BaseModel):
@@ -22,11 +29,54 @@ def sign_jwt(user_id: int) -> JWTToken:
     now = time.time()
     payload = {
         "iss": "curso-fastapi.com.br",
-        "sub": user_id,
+        "sub": str(user_id),
         "aud": "curso-fastapi",
-        "exp": now * (60 * 30), # 30 min         
-        "iat": now,
-        "nbf": now,
-        "jti": uuid4().hex
+        "exp": int(now + (60 * 30)),  # 30 minutes
+        "iat": int(now),
+        "nbf": int(now),
+        "jti": uuid4().hex,
     }
     token = jwt.encode(payload, SECRET, algorithm=ALGORITHM)
+    return {"access_token": token}
+
+
+async def decode_jwt(token: str) -> JWTToken | None:
+    try:
+        decoded_token = jwt.decode(token, SECRET, audience="curso-fastapi", algorithms=[ALGORITHM])
+        _token = JWTToken.model_validate({"access_token": decoded_token})
+        return _token if _token.access_token.exp >= time.time() else None
+    except Exception:
+        return None
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> JWTToken:
+        authorization = request.headers.get("Authorization", "")
+        scheme, _, credentials = authorization.partition(" ")
+        print(scheme)
+        print(credentials)
+
+        if credentials:
+            if not scheme == "Bearer":
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication scheme.")
+
+            payload = await decode_jwt(credentials)
+            print(payload)
+            if not payload:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token.")
+            return payload
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authorization code.")
+
+
+async def get_current_user(token: Annotated[JWTToken, Depends(JWTBearer())]) -> dict[str, int]:
+    return {"user_id": token.access_token.sub}
+
+
+def login_required(current_user: Annotated[dict[str, int], Depends(get_current_user)]):
+    if not current_user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
+    return current_user
